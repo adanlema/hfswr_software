64acd6917a236a4fa2e354b1a75fc135b83e8fa8
adanlema <adanlema@hotmail.com>
2024-05-19 21:49:28 -0300

    reordenamiento del codigo

--- receptor/src/main.c
+++ receptor/src/main.c
@@ -24,14 +24,15 @@
 #define LAST_ADDR      3
 #define BUFFER_TO_READ 4
 #define LOST_DATA      5
+#define START_FPGA     6
 
 #define EXT_ERR_CREATE_SERVER  1
 #define EXT_ERR_CLIENT_CONNECT 2
 #define EXT_ERR_LISTENING_SOCK 3
 /*==================[internal data declaration]==============================*/
 static int     server_sock, client_sock;
-static addrs_t addr_buff_1 = NULL;
-static addrs_t addr_buff_2 = NULL;
+static addrs_t addr_buff_1    = NULL;
+static addrs_t addr_buff_2    = NULL;
 static addrs_t addr_ctrl_regs = NULL;
 /*==================[internal functions declaration]=========================*/
 static void MySignalHandler(int sig);
@@ -52,9 +53,9 @@ static void MySignalHandler(int sig) {
 }
 
 static void serverClientManagement(int confd) {
-    char *s_buff;
-    int buff_leido = 0;
-    int i = 0;
+    char * s_buff;
+    int    buff_leido = 0;
+    int    i          = 0;
 
     s_buff = malloc(BUFTCP_SIZE);
     if (s_buff == NULL) {
@@ -63,21 +64,23 @@ static void serverClientManagement(int confd) {
     }
     memset(s_buff, 0, BUFTCP_SIZE);
 
-    while(i<5){
-        if(buff_leido != addr_ctrl_regs[BUFFER_TO_READ]){
+    while (i < 5) {
+        if (buff_leido != addr_ctrl_regs[BUFFER_TO_READ]) {
             buff_leido = addr_ctrl_regs[BUFFER_TO_READ];
             switch (addr_ctrl_regs[BUFFER_TO_READ]) {
                 case 1:
                     addr_ctrl_regs[WRITE_EN] = (uint32_t)(0xFFFFFFFE & (addr_ctrl_regs[WRITE_EN]));
-                    sprintf(s_buff,"\nDatos buffer uno:");
+                    sprintf(s_buff, "\nDatos buffer uno:");
                     send(confd, s_buff, BUFTCP_SIZE, 0);
                     for (addrs_t p = addr_buff_1; p < (addr_buff_1 + 200); ++p) {
-                    // for (addrs_t p = addr_buff_1; p < (addr_buff_1 + (addr_ctrl_regs[LAST_ADDR] == 0xFFFFFFFF)? 4095 : addr_ctrl_regs[LAST_ADDR]); ++p) {
+                        // for (addrs_t p = addr_buff_1; p < (addr_buff_1 +
+                        // (addr_ctrl_regs[LAST_ADDR] == 0xFFFFFFFF)? 4095 :
+                        // addr_ctrl_regs[LAST_ADDR]); ++p) {
                         sprintf(s_buff, "\n%d", (int16_t)(0xFFFF & (*p)));
                         send(confd, s_buff, BUFTCP_SIZE, 0);
                     }
                     if (addr_ctrl_regs[LOST_DATA]) {
-                        sprintf(s_buff,"\nSe Perdieron datos!!");
+                        sprintf(s_buff, "\nSe Perdieron datos!!");
                         send(confd, s_buff, BUFTCP_SIZE, 0);
                     }
                     addr_ctrl_regs[WRITE_EN] = (uint32_t)(0x1 | (addr_ctrl_regs[WRITE_EN]));
@@ -87,7 +90,8 @@ static void serverClientManagement(int confd) {
                     addr_ctrl_regs[WRITE_EN] = (uint32_t)(0xFFFFFFFD & (addr_ctrl_regs[WRITE_EN]));
                     sprintf(s_buff, "\nDatos buffer dos:");
                     send(confd, s_buff, BUFTCP_SIZE, 0);
-                    // for (addrs_t p = addr_buff_2; p < (addr_buff_2 + (addr_ctrl_regs[LAST_ADDR] == 0xFFFFFFFF)? 4095 : addr_ctrl_regs[LAST_ADDR]); ++p) {
+                    // for (addrs_t p = addr_buff_2; p < (addr_buff_2 + (addr_ctrl_regs[LAST_ADDR]
+                    // == 0xFFFFFFFF)? 4095 : addr_ctrl_regs[LAST_ADDR]); ++p) {
                     for (addrs_t p = addr_buff_2; p < (addr_buff_2 + 200); ++p) {
                         sprintf(s_buff, "\n%d", (int16_t)(0xFFFF & (*p)));
                         send(confd, s_buff, BUFTCP_SIZE, 0);
@@ -110,7 +114,7 @@ static void serverClientManagement(int confd) {
 
 /*==================[external functions definition]==========================*/
 int main() {
-    int n;
+    int                n;
     struct sockaddr_in client;
 
     log_delete();
@@ -143,18 +147,21 @@ int main() {
 
     // Manejo de señales de linux...
     signal(SIGABRT, MySignalHandler);
-    signal(SIGINT,  MySignalHandler);
+    signal(SIGINT, MySignalHandler);
     signal(SIGTERM, MySignalHandler);
     signal(SIGKILL, MySignalHandler);
 
-    //Valores iniciales
-    // addr_ctrl_regs[PHASE_CARRIER] = 0x14d5555c; // Frecuencia de portadora en 10 MHz - No deberia tocar esto desde aqui!!! 
-    addr_ctrl_regs[ADDR_RESET]    = 1;
-    addr_ctrl_regs[WRITE_EN]      = 3;
+    // Valores iniciales
+    // addr_ctrl_regs[PHASE_CARRIER] = 0x14d5555c; // Frecuencia de portadora en 10 MHz - No deberia
+    // tocar esto desde aqui!!!
+    addr_ctrl_regs[START_FPGA] = 0;
+    addr_ctrl_regs[ADDR_RESET] = 1;
+    addr_ctrl_regs[WRITE_EN]   = 3;
+    addr_ctrl_regs[START_FPGA] = 1;
 
     while (1) {
         // Conexion con el cliente...
-        n = sizeof(client);
+        n           = sizeof(client);
         client_sock = accept(server_sock, (struct sockaddr *)&client, &n);
         if (client_sock < 0) {
             log_add("[ERROR]Error al conectar el cliente");
